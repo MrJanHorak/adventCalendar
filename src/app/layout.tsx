46e5eb52f66789f86f5c830045b27e89a1aa1ce6
MrJanHorak <janny_b_goode@hotmail.com>
2025-11-30 20:22:26 -0500

    add analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
+import Script from 'next/script';
 import './globals.css';
 import CharityBanner from '@/components/CharityBanner';
 
@@ -26,7 +27,7 @@ export const metadata: Metadata = {
     siteName: 'Doorly Advent',
     images: [
       {
-        url: '/og-image.png', // You'll need to create this image
+        url: '/og-image.png',
         width: 1200,
         height: 630,
         alt: 'Doorly Advent - Personalized Advent Calendars',
@@ -40,7 +41,7 @@ export const metadata: Metadata = {
     title: 'Doorly Advent - Create Your Magical Calendar',
     description:
       'Create and share personalized advent calendars with your loved ones. Open a new surprise each day in December!',
-    images: ['/og-image.png'], // Same image as Open Graph
+    images: ['/og-image.png'],
   },
 };
 
@@ -51,6 +52,21 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='en'>
+      <head>
+        {/* Google Analytics */}
+        <Script
+          strategy='afterInteractive'
+          src='https://www.googletagmanager.com/gtag/js?id=G-F7Q8ZBCPYJ'
+        />
+        <Script id='google-analytics' strategy='afterInteractive'>
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-F7Q8ZBCPYJ');
+          `}
+        </Script>
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
